d8f88fb85cc07c7c88d35ef5ff09c97f570d3209
Fukongfred <147664394+Fukongfred@users.noreply.github.com>
2024-03-02 18:21:50 -0800

    Add files via upload

---
+++ 514 final sensing device/src/main.cpp
@@ -0,0 +1,209 @@
+#include <Adafruit_GFX.h>
+#include <Arduino.h>
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+#include <BLE2902.h>
+#include <stdlib.h>
+
+// Variables for the buzzer
+const int buzzerPin = D0; // Adjust to your actual pin for the buzzer
+const int fsrPin = D1; // Adjust to your actual analog pin for the FSR
+bool buzzerActive = false; // To track the buzzer state
+
+// Threshold for FSR reading to stop the buzzer
+int initialWeight = 0;
+int currentWeight = 0;
+unsigned long lastTime = 0;
+const int sampleInterval = 1000; 
+
+// TODO: change the service UUID to the one you are using on the server side.
+// The remote service we wish to connect to.
+static BLEUUID serviceUUID("cbc8b292-c579-417d-bdae-9219a6362a83");
+// The characteristic of the remote service we are interested in.
+static BLEUUID    charUUID("5a645de1-e060-40f5-9745-646e2b954b27");
+
+static boolean doConnect = false;
+static boolean connected = false;
+static boolean doScan = false;
+static BLERemoteCharacteristic* pRemoteCharacteristic;
+static BLEAdvertisedDevice* myDevice;
+
+static void notifyCallback(
+  BLERemoteCharacteristic* pBLERemoteCharacteristic,
+  uint8_t* pData,
+  size_t length,
+  bool isNotify) {
+    // TODO: add codes to handle the data received from the server, and call the data aggregation function to process the data
+
+    // TODO: change the following code to customize your own data format for printing
+    Serial.print("Notify callback for characteristic ");
+    Serial.print(pBLERemoteCharacteristic->getUUID().toString().c_str());
+    Serial.print(" of data length ");
+    Serial.println(length);
+    Serial.print("data: ");
+    Serial.write(pData, length);
+    Serial.println();
+    Serial.println("Activating buzzer...");
+    // Activate the buzzer
+    digitalWrite(buzzerPin, HIGH);
+    buzzerActive = true;
+}
+
+class MyClientCallback : public BLEClientCallbacks {
+  void onConnect(BLEClient* pclient) {
+  }
+
+  void onDisconnect(BLEClient* pclient) {
+    connected = false;
+    Serial.println("onDisconnect");
+  }
+};
+
+bool connectToServer() {
+    Serial.print("Forming a connection to ");
+    Serial.println(myDevice->getAddress().toString().c_str());
+
+    BLEClient*  pClient  = BLEDevice::createClient();
+    Serial.println(" - Created client");
+
+    pClient->setClientCallbacks(new MyClientCallback());
+
+    // Connect to the remove BLE Server.
+    pClient->connect(myDevice);  // if you pass BLEAdvertisedDevice instead of address, it will be recognized type of peer device address (public or private)
+    Serial.println(" - Connected to server");
+    pClient->setMTU(517); //set client to request maximum MTU from server (default is 23 otherwise)
+
+    // Obtain a reference to the service we are after in the remote BLE server.
+    BLERemoteService* pRemoteService = pClient->getService(serviceUUID);
+    if (pRemoteService == nullptr) {
+      Serial.print("Failed to find our service UUID: ");
+      Serial.println(serviceUUID.toString().c_str());
+      pClient->disconnect();
+      return false;
+    }
+    Serial.println(" - Found our service");
+
+    // Obtain a reference to the characteristic in the service of the remote BLE server.
+    pRemoteCharacteristic = pRemoteService->getCharacteristic(charUUID);
+    if (pRemoteCharacteristic == nullptr) {
+      Serial.print("Failed to find our characteristic UUID: ");
+      Serial.println(charUUID.toString().c_str());
+      pClient->disconnect();
+      return false;
+    }
+    Serial.println(" - Found our characteristic");
+
+    // Read the value of the characteristic.
+    if(pRemoteCharacteristic->canRead()) {
+      std::string value = pRemoteCharacteristic->readValue();
+      Serial.print("The characteristic value was: ");
+      Serial.println(value.c_str());
+    }
+
+    if(pRemoteCharacteristic->canNotify())
+      pRemoteCharacteristic->registerForNotify(notifyCallback);
+
+    connected = true;
+    return true;
+}
+/**
+ * Scan for BLE servers and find the first one that advertises the service we are looking for.
+ */
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+  /**
+   * Called for each advertising BLE server.
+   */
+  void onResult(BLEAdvertisedDevice advertisedDevice) {
+    Serial.print("BLE Advertised Device found: ");
+    Serial.println(advertisedDevice.toString().c_str());
+
+    // We have found a device, let us now see if it contains the service we are looking for.
+    if (advertisedDevice.haveServiceUUID() && advertisedDevice.isAdvertisingService(serviceUUID)) {
+
+      BLEDevice::getScan()->stop();
+      myDevice = new BLEAdvertisedDevice(advertisedDevice);
+      doConnect = true;
+      doScan = true;
+
+    } // Found our server
+  } // onResult
+}; // MyAdvertisedDeviceCallbacks
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Starting Arduino BLE Client application...");
+  BLEDevice::init("");
+
+  // Retrieve a Scanner and set the callback we want to use to be informed when we
+  // have detected a new device.  Specify that we want active scanning and start the
+  // scan to run for 5 seconds.
+  BLEScan* pBLEScan = BLEDevice::getScan();
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setInterval(1349);
+  pBLEScan->setWindow(449);
+  pBLEScan->setActiveScan(true);
+  pBLEScan->start(5, false);
+
+   // Initialize the buzzer pin as output
+  pinMode(buzzerPin, OUTPUT);
+  digitalWrite(buzzerPin, LOW); // Ensure buzzer is off initially
+  pinMode(fsrPin, INPUT);
+  initialWeight = analogRead(fsrPin);
+  lastTime = millis();
+} // End of setup.
+
+// This is the Arduino main loop function.
+void loop() {
+  // If the flag "doConnect" is true then we have scanned for and found the desired
+  // BLE Server with which we wish to connect.  Now we connect to it.  Once we are
+  // connected we set the connected flag to be true.
+
+  if (doConnect == true) {
+    if (connectToServer()) {
+      Serial.println("We are now connected to the BLE Server.");
+    } else {
+      Serial.println("We have failed to connect to the server; there is nothin more we will do.");
+    }
+    doConnect = false;
+  }
+ 
+  // If we are connected to a peer BLE Server, update the characteristic each time we are reached
+  // with the current time since boot.
+  if (connected) {
+    String newValue = "Time since boot: " + String(millis()/1000);
+    Serial.println("Setting new characteristic value to \"" + newValue  + "\"");
+
+    // Set the characteristic's value to be the array of bytes that is actually a string.
+    pRemoteCharacteristic->writeValue(newValue.c_str(), newValue.length());
+  }else if(doScan){
+    BLEDevice::getScan()->start(0);  // this is just example to start scan after disconnect, most likely there is better way to do it in arduino
+  }
+
+  unsigned long currentTime = millis();
+
+  if (currentTime - lastTime >= sampleInterval) {
+    // It's time to calculate the weight change average
+    currentWeight = analogRead(fsrPin);
+    int weightChange = currentWeight - initialWeight;
+
+        // Print the current weight and weight change to the Serial Monitor
+    if (abs(currentWeight) > 4000) {
+      weightChange = 10;
+    }
+    Serial.print("Current Weight: ");
+    Serial.print(currentWeight);
+    Serial.print("g | Weight Change: ");
+    Serial.println(weightChange);
+
+    if (abs(weightChange) > 400) {
+      Serial.println("Weight detected on FSR, stopping buzzer...");
+      digitalWrite(buzzerPin, LOW);
+      buzzerActive = false; // Reset the buzzer state
+    }
+
+  delay(1000); 
+  }// Delay a second between loops.
+} // End of loop
